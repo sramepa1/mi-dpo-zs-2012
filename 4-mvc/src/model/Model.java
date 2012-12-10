1f8a236b09f6455460094e374b59e22d2b5623fa
rusty <rusty@rusty-AMILO-Xi-2528>
2012-12-10 19:12:50 +0100

    Initial work at model.

---
+++ 4-mvc/src/model/Model.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JTable;
+
+
+/**
+ *
+ * @author rusty
+ */
+public class Model {
+    
+    Model() {
+        shapeTypes = new HashMap<String, ArrayList<Shape>>();
+    }
+    
+    private HashMap<String, ArrayList<Shape>> shapeTypes;
+    
+    public void addShapeType(String typeName) {
+        if(shapeTypes.containsKey(typeName)) {
+            throw new IllegalArgumentException("This type of shapes already exists.");
+        }
+        
+        shapeTypes.put(typeName, new ArrayList<Shape>());
+    }
+    
+    public void addShape(String typeName, Shape shape) {
+        if(!shapeTypes.containsKey(typeName)) {
+            throw new IllegalArgumentException("Unknown shape type.");
+        }
+        
+        shapeTypes.get(typeName).add(shape);
+    }
+    
+    public void clearAll() {
+        for (Map.Entry<String, ArrayList<Shape>> entry : shapeTypes.entrySet())
+        {
+            entry.setValue(new ArrayList<Shape>());
+        }
+    }
+    
+    public Iterable<String> getShapeTypes() {
+        LinkedList<String> types = new LinkedList<String>();
+        
+        for (Map.Entry<String, ArrayList<Shape>> entry : shapeTypes.entrySet())
+        {
+            types.add(entry.getKey());
+        }
+        
+        return types;
+    }
+    
+    public Shape getShape(String typeName, int id) {   
+        if(!shapeTypes.containsKey(typeName)) {
+            throw new IllegalArgumentException("Unknown shape type.");
+        }
+        
+        return shapeTypes.get(typeName).get(id);
+    }
+    
+    public final Iterable<Shape> getAllShapes(String typeName) {
+        if(!shapeTypes.containsKey(typeName)) {
+            throw new IllegalArgumentException("Unknown shape type.");
+        }
+        
+        return shapeTypes.get(typeName);
+    }
+
+}
---
+++ 4-mvc/src/model/Shape.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author rusty
+ */
+public abstract class Shape {
+    
+    public abstract int getX();
+    public abstract int getY();
+    
+}
